32c23f9ddafff7d81b3280646b645dc4e8005d0a
larky0071 <larky0071@gmail.com>
2022-04-25 14:16:32 +0300

    add: axios requests

--- frontend/src/pages/Add/Add.tsx
+++ frontend/src/pages/Add/Add.tsx
@@ -2,7 +2,9 @@ import { Box, Tab, TextField, ThemeProvider, createTheme, Button } from '@mui/ma
 import { TabContext, TabPanel, TabList } from '@mui/lab';
 import s from './Add.module.css'
 import { useState } from 'react';
+import axios from 'axios';
 
+//TODO: refactor
 function Add() {
     const [currentTab, setCurrentTab] = useState("discord")
     const [SRVID, setSRVID] = useState("")
@@ -13,14 +15,19 @@ function Add() {
     const changeSRVLink = (e: any) => setSRVLink(e.target.value)
     const changeSRVAbout = (e: any) => setSRVAbout(e.target.value)
 
-    const validSRVID = () => {
-        if (SRVID !== "" || SRVID.match(/[a-zA-ZА-Яа-я]/g) || SRVID.length !== 18) return false
-        return true
-    }
-
     const theme = createTheme({
         palette: { mode: "dark" }
     });
+
+    //TODO: add antd alert to .then & .catch
+    const sendForm = () => {
+        axios({
+            'url': `http://rcff.ru/api/new?id=${SRVID}&invite=${SRVLink}&description=${SRVAbout}`,
+            'method': 'POST'
+        }).then(res => {
+            if (res.data === true) alert("Your server has been sended to us") 
+        })
+    }
 
     return <div className={s.Page}>
         <div className={s.PageContentWrapper}>
@@ -41,7 +48,7 @@ function Add() {
                         <TabPanel value="discord">
                             <div className={`${s.Tab} ${s.Discord}`}>
                                 <div className={s.Row}>
-                                    <TextField className={s.Row} error={!validSRVID()} label="Server ID (match 0-9, 18 characters)" variant="outlined" size='small' value={SRVID} onChange={changeSRVID} />
+                                    <TextField className={s.Row} label="Server ID (match 0-9, 18 characters)" variant="outlined" size='small' value={SRVID} onChange={changeSRVID} />
                                 </div>
                                 <div className={s.Row}>
                                     <TextField className={s.Row} label="Invite Link (example: https://discord.gg/r97sCPz)" variant="outlined" size='small' value={SRVLink} onChange={changeSRVLink} />
@@ -49,7 +56,7 @@ function Add() {
                                 <div className={s.Row}>
                                     <TextField className={s.Row} variant='outlined' label="About Server" multiline rows={4} value={SRVAbout} onChange={changeSRVAbout} />
                                 </div>
-                                <Button variant="outlined">Отправить заявку</Button>
+                                <Button variant="outlined" onClick={sendForm}>Отправить заявку</Button>
                             </div>
 
                         </TabPanel>
