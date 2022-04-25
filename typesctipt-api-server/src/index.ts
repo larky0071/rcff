22622703a4e1ada1376affc613e66522fb593e08
larky0071 <larky0071@gmail.com>
2022-04-25 22:20:12 +0300

    add: mysql2

--- typesctipt-api-server/src/index.ts
+++ typesctipt-api-server/src/index.ts
@@ -1,4 +1,8 @@
-const PORT: number = 8081
+import mysql from 'mysql2'
+import express from 'express'
+import colors from 'colors'
+
+const config = require('./config.json')
 
 type Nullable<T> = T | null
 
@@ -16,11 +20,11 @@ interface IGuild {
 
 class Guild implements IGuild {
     id: number
-    name: string
-    avatar: string
-    banner: string
-    online: number
-    offline: number
+    name: Nullable<string> = null
+    avatar: Nullable<string> = null
+    banner: Nullable<string> = null
+    online: Nullable<number> = null
+    offline: Nullable<number> = null
     invite: string
     description: string
 
@@ -32,11 +36,42 @@ class Guild implements IGuild {
     }
 }
 
-class DataBase {
-    Guilds: Array<Guild>
+class DatabaseManager {
+    connection: mysql.Connection
+    Guilds: Array<IGuild> = []
+
+    constructor() {
+        try {
+            console.log("Trying connect database...")
+            this.connection = mysql.createConnection(config.mysql)
+
+            console.log(colors.green("Database connected!"))
+            console.log("Search tables...")
+            this.connection.execute(`SHOW TABLES LIKE '${"guilds"}'`, (err, result, field) => {
+                if (Array.isArray(result) && result.length > 0) return console.log(colors.green("Table 'guilds' exist!"))
+                console.log(colors.red("Can't find table 'guilds', creating..."))
+                this.connection.execute('CREATE TABLE IF NOT EXISTS `guilds` (`id` INT NOT NULL, `name` TEXT, `avatar` TEXT, `banner` TEXT, `online` INT, `offline` INT, `invite` TEXT NOT NULL, `description` TEXT NOT NULL, PRIMARY KEY (`id`))', (err, result, field) => {
+                    if (!err) console.log(colors.green("Table 'accepted' created!"))
+                })
+            })
+
+            this.connection.execute(`SHOW TABLES LIKE '${"accepted"}'`, (err, result, field) => {
+                if (Array.isArray(result) && result.length > 0) return console.log(colors.green("Table 'accepted' exist!"))
+                console.log(colors.red("Can't find table 'accepted', creating..."))
+                this.connection.execute('CREATE TABLE IF NOT EXISTS `accepted` (`id` INT NOT NULL, `accepted` BOOLEAN, PRIMARY KEY (`id`))', (err, result, field) => {
+                    if (!err) console.log(colors.green("Table 'accepted' created!"))
+                })
+            })
+
+        } catch (err) {
+            throw Error(err)
+        }
+    }
 
     public returnAllData() {
-        return { 'GuildCollection': this.Guilds }
+        return {
+            'GuildCollection': this.Guilds
+        }
     }
 
     public newGuild(id: number, invite: string, description: string) {
@@ -56,13 +91,14 @@ class DataBase {
 
 }
 
-import express from 'express'
 const app = express()
-
-const db = new DataBase()
+const db = new DatabaseManager()
 
 //TODO: CHANGE 'GET' TO 'POST' IF NEEDED
 //TODO: FIX TYPES FOR 'req', 'res' to change 'any' => 'express.Request', 'express.Response'
+
+//todo: rework to open api docs
+app.all('/api', (req: any, res: any) => res.send({ 'API': db.returnAllData() }))
 
 //todo: add validation when connect to mysql
 app.post('/api/list/accept', (req: express.Request, res: express.Response) => {
@@ -71,11 +107,6 @@ app.post('/api/list/accept', (req: express.Request, res: express.Response) => {
 
 //todo: add validation when connect to mysql
 app.post('/api/list/checking', (req: any, res: any) => {
-    res.send(db.returnAllData())
-})
-
-//todo: rework to open api docs
-app.all('/api', (req: any, res: any) => {
     res.send(db.returnAllData())
 })
 
@@ -95,4 +126,4 @@ app.post('/api/guild/modify', (req: any, res: any) => {
     res.send(true)
 })
 
-app.listen(PORT)
+app.listen(8081, () => console.log("API server now listen on port", 8081))
