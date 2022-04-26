309d4d328053b5e50860f8817b957b9478830e35
larky0071 <larky0071@gmail.com>
2022-04-26 17:18:45 +0300

    fix: InfoPanel text

--- frontend/src/components/InfoPanel/InfoPanel.tsx
+++ frontend/src/components/InfoPanel/InfoPanel.tsx
@@ -3,9 +3,9 @@ import s from './InfoPanel.module.css'
 function InfoPanel() {
     return <div className={s.InfoPanel}>
         <div className={s.InfoWrapper}>
-            <h2>Список фурри дискорд серверов</h2>
+            <h2>Список Discord-серверов по тематике фурри</h2>
             <div className={s.sp}></div>
-            <p>Собрание РУ язычных фурри серверов</p>
+            <p>Чтобы добавить свой сервер, нажмите на соответствующую кнопку для подачи анкеты, а также свяжитесь с нами в Discord: n2k#9665</p>
         </div>
         <div className={s.StatWrapper}>
             <p><span>1167</span> - Серверов | <span>15367</span> - Участников</p>
